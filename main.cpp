1c328a816ca57793671047352183c253463a58b3
pinnacleteamjordan <jordan.selanders@pinnaclewealth.ca>
2017-10-26 15:15:00 -0600

    No large changes, only minor errors

--- main.cpp
+++ main.cpp
@@ -53,29 +53,33 @@ class SQL {
             }
             PQclear(res);
 
-            std::string command = "DECLARE myportal CURSOR FOR " + sql;
-            res = PQexec(conn, command.c_str());
-            if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-                fprintf(stderr, "DECLARE CURSOR failed: %s", PQerrorMessage(conn));
-                PQclear(res);
-                exit_nicely(conn);
-            }
-            PQclear(res);
-
-            res = PQexec(conn, "FETCH ALL in myportal");
-            if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-                fprintf(stderr, "FETCH ALL failed: %s", PQerrorMessage(conn));
-                PQclear(res);
-                exit_nicely(conn);
-            }
-            nFields = PQnfields(res);
-
-            for (x = 0; x < PQntuples(res); x++) {
-                for (i = 0; i < nFields; i++) {
-                    std::string str_a = PQfname(res, i);
-                    std::string str_b = PQgetvalue(res, x, i);
-                    returnData.insert(std::pair<std::string, std::string>(str_a, str_b));
+            if (std::regex_match(sql, re)) {
+                std::string command = "DECLARE myportal CURSOR FOR " + sql;
+                res = PQexec(conn, command.c_str());
+                if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+                    fprintf(stderr, "DECLARE CURSOR failed: %s", PQerrorMessage(conn));
+                    PQclear(res);
+                    exit_nicely(conn);
                 }
+                PQclear(res);
+
+                res = PQexec(conn, "FETCH ALL in myportal");
+                if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+                    fprintf(stderr, "FETCH ALL failed: %s", PQerrorMessage(conn));
+                    PQclear(res);
+                    exit_nicely(conn);
+                }
+                nFields = PQnfields(res);
+
+                for (x = 0; x < PQntuples(res); x++) {
+                    for (i = 0; i < nFields; i++) {
+                        std::string str_a = PQfname(res, i);
+                        std::string str_b = PQgetvalue(res, x, i);
+                        returnData.insert(std::pair<std::string, std::string>(str_a, str_b));
+                    }
+                }
+            } else {
+                res = PQexec(conn, sql.c_str());
             }
 
             PQclear(res);
@@ -154,7 +158,7 @@ int main() {
             ctx["navigation"] = crow::mustache::load("navigation.html").render();
 
             return crow::mustache::load("dashboard.html").render(ctx);
-    });
+            });
 
     CROW_ROUTE(app, "/dashboard/representatives")([](){
             crow::mustache::context ctx;
@@ -163,7 +167,7 @@ int main() {
             ctx["navigation"] = crow::mustache::load("navigation.html").render();
 
             return crow::mustache::load("representatives.html").render(ctx);
-    });
+            });
 
     CROW_ROUTE(app, "/get/reps/init").methods("GET"_method)([]{
             std::map<std::string, std::string>::iterator mapIterator;
@@ -175,11 +179,11 @@ int main() {
 
             mapIterator = sqlValue.begin();
             for (mapIterator=sqlValue.begin(); mapIterator!=sqlValue.end(); mapIterator++) {
-                json[mapIterator->first] = mapIterator->second;
+            json[mapIterator->first] = mapIterator->second;
             }
 
             return json;
-    });
+            });
 
     CROW_ROUTE(app, "/get/reps/info").methods("GET"_method)([]{
             std::map<std::string, std::string>::iterator mapIterator;
@@ -191,28 +195,28 @@ int main() {
 
             mapIterator = sqlValue.begin();
             for (mapIterator=sqlValue.begin(); mapIterator!=sqlValue.end(); mapIterator++) {
-                json[mapIterator->first] = mapIterator->second;
+            json[mapIterator->first] = mapIterator->second;
             }
 
             return json;
-    });
+            });
 
     app.route_dynamic("/post/reps/info").methods(crow::HTTPMethod::POST)([](const crow::request& req) {
             auto x = crow::json::load(req.body);
             if (!x)
-                return crow::response(400);
+            return crow::response(400);
 
             std::ostringstream os;
 
             os << "UPDATE data_representatives SET firstname=" << x["firstname"]
-               << ", lastname=" << x["lastname"]
-               << ", title=" << x["title"]
-               << ", email=" << x["email"]
-               << ", phone=" << x["phone"]
-               << ", workphone=" << x["workphone"]
-               << ", cellphone=" << x["cellphone"]
-               << ", faxnumber=" << x["faxnumber"]
-               << " AND id=" << x["id"];
+            << ", lastname=" << x["lastname"]
+            << ", title=" << x["title"]
+            << ", email=" << x["email"]
+            << ", phone=" << x["phone"]
+            << ", workphone=" << x["workphone"]
+            << ", cellphone=" << x["cellphone"]
+            << ", faxnumber=" << x["faxnumber"]
+            << " AND id=" << x["id"];
 
             SQL sql;
             sql.Run(os.str());
@@ -227,19 +231,19 @@ int main() {
     app.route_dynamic("/post/reps/new").methods(crow::HTTPMethod::POST)([](const crow::request& req) {
             auto x = crow::json::load(req.body);
             if (!x)
-                return crow::response(400);
+            return crow::response(400);
 
             std::ostringstream os;
 
             os << "INSERT INTO data_representatives (firstname, lastname, title, email, phone, workphone, "
-               << "cellphone, faxnumber) VALUES (" << x["firstname"]
-               << ", " << x["lastname"]
-               << ", " << x["title"]
-               << ", " << x["email"]
-               << ", " << x["phone"]
-               << ", " << x["workphone"]
-               << ", " << x["cellphone"]
-               << ", " << x["faxnumber"];
+            << "cellphone, faxnumber) VALUES (" << x["firstname"]
+            << ", " << x["lastname"]
+            << ", " << x["title"]
+            << ", " << x["email"]
+            << ", " << x["phone"]
+            << ", " << x["workphone"]
+            << ", " << x["cellphone"]
+            << ", " << x["faxnumber"];
 
             crow::json::wvalue json;
             json["status"] = "Complete";
